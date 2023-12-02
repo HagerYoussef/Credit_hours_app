23913deb67b8ac9dcf58f5bf0fd1f489e208f192
HagerYoussef <hageryossef5@gmail.com>
2023-12-02 20:37:06 +0200

    Sarting point

---
+++ lib/Views/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String routeName='Login Screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:credit_hour_app/Views/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName:(_)=>LoginScreen(),
+      },
+    );
+  }
+}
